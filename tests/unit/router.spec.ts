80f7463b2d521525b596ef97e8fe6ffed0e7f627
Aurelien ARINO <aurelien.arino@gmail.com>
2019-07-29 16:58:40 +0200

    ci(circleci): add circleci config
    install, lint, test, build

--- tests/unit/router.spec.ts
+++ tests/unit/router.spec.ts
@@ -2,6 +2,5 @@ describe('router.ts', () => {
   it('should have routes', () => {
     const router = require('../../src/router')
     expect(router.default.options.routes).toBeDefined()
-    console.log(router.default.options.routes)
   })
 })
--- tests/unit/views/Home.spec.ts
+++
@@ -1,9 +0,0 @@
-import { shallowMount, mount } from '@vue/test-utils'
-import Home from '@/views/Home.vue'
-
-describe('Home.vue', () => {
-  it('should display an HelloWorld message', () => {
-    const home = mount(Home)
-    expect(home.html()).toBeTruthy()
-  })
-})
