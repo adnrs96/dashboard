12f70275bb3ac533331fa81ca71ba193fb8cf660
Aurelien ARINO <aurelien.arino@gmail.com>
2019-08-05 14:37:40 +0200

    feat(loader): add loader component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="dashboard-container"></div>
+  <div class="dashboard-container">
+    <!-- <loader /> -->
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import Loader from '@/components/Loader.vue'
 
-@Component({})
+@Component({
+  components: {
+    Loader
+  }
+})
 export default class Home extends Vue { }
 </script>
