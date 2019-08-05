f972a95f9e533f70f205281e19957e6594e27f11
Aurelien ARINO <aurelien.arino@gmail.com>
2019-08-05 18:56:17 +0200

    feat(components): add logo component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,10 @@
 <template>
-  <div class="dashboard-container">
-    <!-- <s-loader /> -->
-  </div>
+  <div class="dashboard-container"></div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import SLoader from '@/components/Loader.vue'
 
-@Component({
-  components: {
-    SLoader
-  }
-})
+@Component({})
 export default class Home extends Vue { }
 </script>
