d8ee2678b18839287eb00a1d125fa522885090ab
Aurelien ARINO <aurelien.arino@gmail.com>
2019-08-07 18:39:41 +0200

    chore(components): init input component

---
+++ src/components/Input.vue
@@ -0,0 +1,58 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'SInput'
+})
+export default class Input extends Vue {
+  @Prop({
+    type: Boolean,
+    default: undefined
+  }) readonly disabled!: boolean | undefined
+
+  @Prop({
+    type: String,
+    default: 'text',
+    validator: x => ['text', 'email', 'password', 'tel', 'number'].includes(x)
+  }) readonly type!: string
+
+  @Prop({
+    type: String,
+    default: ''
+  }) readonly value!: string
+
+  @Prop({
+    type: String,
+    default: undefined
+  }) readonly placeholder!: string | undefined
+
+  @Prop({
+    type: String,
+    default: undefined
+  }) readonly name!: string | undefined
+
+  @Prop({
+    type: Boolean,
+    default: false
+  }) readonly required!: boolean
+
+  @Prop({
+    type: Boolean,
+    default: false
+  }) readonly readonly!: boolean
+
+  @Prop({
+    type: Boolean,
+    default: false
+  }) readonly loading!: boolean
+
+  @Prop({
+    type: Boolean,
+    default: undefined
+  }) readonly valid!: boolean
+}
+</script>
