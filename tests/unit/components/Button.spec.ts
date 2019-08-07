bdd75f27f1fb305887a8dcc81f1fa8dd626d9194
Aurelien ARINO <aurelien.arino@gmail.com>
2019-08-07 15:35:32 +0200

    test(components): 100% coverage on components

--- tests/unit/components/Button.spec.ts
+++ tests/unit/components/Button.spec.ts
@@ -2,19 +2,51 @@ import { mount } from '@vue/test-utils'
 import Button from '@/components/Button.vue'
 
 describe('Button.vue', () => {
-  it('should have to good color', () => {
-    const black = mount(Button, {
-      propsData: {
-        black: true
-      }
-    })
-    const white = mount(Button, {
-      propsData: {
-        white: true
-      }
-    })
+  describe('get path()', () => {
+    it('should return the good color', () => {
+      const black = mount(Button, {
+        propsData: {
+          black: true
+        }
+      })
+      const white = mount(Button, {
+        propsData: {
+          white: true
+        }
+      })
 
-    // expect(black.find('p').classes('bg-black')).toBeTruthy()
-    // expect(white.find('p').classes('bg-white')).toBeTruthy()
+      expect((black.vm as any).textColor).toBe('white')
+      expect((white.vm as any).textColor).toBe('black')
+    })
+  })
+
+  describe('get textColor', () => {
+    it('should return the good icon', () => {
+      const github = mount(Button, {
+        propsData: {
+          icon: 'github-o'
+        }
+      })
+      const gitlab = mount(Button, {
+        propsData: {
+          icon: 'gitlab-o'
+        }
+      })
+      const google = mount(Button, {
+        propsData: {
+          icon: 'google-f'
+        }
+      })
+
+      expect((github.vm as any).path).toBe(
+        './assets/img/logos/github-waving-cat-outline.svg'
+      )
+      expect((gitlab.vm as any).path).toBe(
+        './assets/img/logos/gitlab-outline.svg'
+      )
+      expect((google.vm as any).path).toBe(
+        './assets/img/logos/google-filled.svg'
+      )
+    })
   })
 })
