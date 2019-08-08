c41ae5e8383d5e410e063345779b02f386b23bea
Aurelien ARINO <aurelien.arino@gmail.com>
2019-08-08 22:38:34 +0200

    feat(create): add arrow for prev in create views

--- src/views/Dashboard/Create/index.vue
+++ src/views/Dashboard/Create/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="flex items-center justify-center">
+    <s-arrow
+      circled
+      left
+      prev
+      class="absolute top-14 left-0 ml-8 mt-8"
+    />
     <p v-if="beginner">
       Welcome
     </p>
@@ -12,9 +18,13 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
+import SArrow from '@/components/Arrow.vue'
 
 @Component({
-  name: 'Create'
+  name: 'Create',
+  components: {
+    SArrow
+  }
 })
 export default class Create extends Vue {
   @Prop({ type: Boolean, default: false }) private beginner!: boolean
