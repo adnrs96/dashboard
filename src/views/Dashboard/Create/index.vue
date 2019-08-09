f2f15bf8d7e03363e6d75bdfbcce766f0733a6cb
Aurelien ARINO <aurelien.arino@gmail.com>
2019-08-09 14:35:57 +0200

    feat(create): add beginner content

--- src/views/Dashboard/Create/index.vue
+++ src/views/Dashboard/Create/index.vue
@@ -1,17 +1,79 @@
 <template>
-  <div class="flex items-start md:items-center justify-center">
+  <div class="flex flex-col items-start md:items-center justify-center">
     <s-arrow
       circled
       left
       prev
+      hover
       class="absolute top-14 left-0 ml-8 mt-8"
     />
-    <p v-if="beginner">
-      Welcome
-    </p>
+    <s-text
+      v-if="beginner"
+      head="1"
+      weight="regular"
+      center
+      class="w-125"
+    >
+      <!-- ToDo Get the user name -->
+      Welcome to Storyscript, Jean Val JeanJean!
+    </s-text>
     <router-view class="m-8" />
-    <footer v-if="beginner">
-      Footer
+    <footer
+      v-if="beginner"
+      class="flex items-center"
+    >
+      <s-text
+        p="3"
+        weight="semibold"
+        center
+        class="mr-12"
+      >
+        Are you looking for help?
+      </s-text>
+      <a
+        href="http://"
+        class="flex items-center"
+      >
+        <s-text
+          p="3"
+          weight="semibold"
+          class="text-indigo-50"
+        >
+          Read our docs
+        </s-text>
+        <s-arrow
+          class="ml-3"
+          top
+          right
+          w="8"
+          h="8"
+          color="#7180FF"
+        />
+      </a>
+      <s-icon
+        icon="dot"
+        class="ml-7 mr-6"
+      />
+      <a
+        href="http://"
+        class="flex items-center"
+      >
+        <s-text
+          p="3"
+          weight="semibold"
+          class="text-indigo-50"
+        >
+          Contact support
+        </s-text>
+        <s-arrow
+          class="ml-3"
+          top
+          right
+          w="8"
+          h="8"
+          color="#7180FF"
+        />
+      </a>
     </footer>
   </div>
 </template>
@@ -19,14 +81,19 @@
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
 import SArrow from '@/components/Arrow.vue'
+import SText from '@/components/Text.vue'
+import SIcon from '@/components/Icon.vue'
 
 @Component({
   name: 'Create',
   components: {
-    SArrow
+    SArrow,
+    SText,
+    SIcon
   }
 })
 export default class Create extends Vue {
-  @Prop({ type: Boolean, default: false }) private beginner!: boolean
+  // ToDo replace true by false
+  @Prop({ type: Boolean, default: true }) private beginner!: boolean
 }
 </script>
